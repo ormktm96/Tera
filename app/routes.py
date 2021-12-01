b08c32715f6fb2694a7b0858cfd6309e65964baf
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2021-12-01 14:53:06 -0500

    Update routes.py
    Receiving login credentials

--- app/routes.py
+++ app/routes.py
@@ -48,3 +48,14 @@ from app.forms import LoginForm
 def login():
     form = LoginForm()
     return render_template('login.html', title='Sign In', form=form)
+
+from flask import render_template, flash, redirect
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)

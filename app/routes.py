283bd31b2412820fa594dce9fc42e1053627a596
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2021-12-01 14:47:35 -0500

    Update routes.py
    Use url\_for() function for links

--- app/routes.py
+++ app/routes.py
@@ -25,3 +25,15 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+from flask import render_template, flash, redirect, url_for
+
+# ...
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # ...
+        return redirect(url_for('index'))
+    # ...

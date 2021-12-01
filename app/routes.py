ea9b02dbd967b1aa9c8ddf9e14ae3a40e5f365df
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2021-12-01 14:52:06 -0500

    Update routes.py
    Login view function

--- app/routes.py
+++ app/routes.py
@@ -37,3 +37,14 @@ def login():
         # ...
         return redirect(url_for('index'))
     # ...
+    
+from flask import render_template
+from app import app
+from app.forms import LoginForm
+
+# ...
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)

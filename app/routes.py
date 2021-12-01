1063f44ea054bd746ad7d646e3fea966b96f9cd3
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2021-12-01 14:37:24 -0500

    Update routes.py
    Fake posts in view function

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,22 @@ from app import app
 def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
+
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)

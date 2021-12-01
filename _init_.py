a765d4e2da6b3cde1d78ddfa58991badfb5d6a3e
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2021-12-01 14:41:19 -0500

    Update _init_.py
    Flask configuration

--- _init_.py
+++ _init_.py
@@ -3,3 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import routes
+
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+from app import routes

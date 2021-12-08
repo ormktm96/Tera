dc79e2b012be42d555e2adc0c4ea27b439ee5584
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2021-12-08 12:02:20 -0500

    Update routes.py
    Chapter 6

--- app/routes.py
+++ app/routes.py
@@ -71,3 +71,11 @@ def login():
         # ...
         return redirect(url_for('index'))
     # ...
+    
+from datetime import datetime
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()

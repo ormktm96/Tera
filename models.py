6addaa5686908600ddb73850adace4f97f679fa5
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2021-12-08 12:00:56 -0500

    Update models.py
    Chapter 6

--- models.py
+++ models.py
@@ -33,3 +33,8 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+    
+class User(UserMixin, db.Model):
+    # ...
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)

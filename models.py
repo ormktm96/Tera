ed61a276c6097c62a38cecfa7b627bc1a5de0ed4
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2021-12-15 13:41:15 -0500

    Update models.py
    Reset password token methods

--- models.py
+++ models.py
@@ -81,3 +81,24 @@ class User(UserMixin, db.Model):
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+from time import time
+import jwt
+from app import app
+
+class User(UserMixin, db.Model):
+    # ...
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)

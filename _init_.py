90e7e0dca6658f18d34fd66287d0394a03ea64e1
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2021-12-22 13:49:53 -0500

    Update _init_.py
    Select best language (Chapter 13)

--- _init_.py
+++ _init_.py
@@ -140,3 +140,8 @@ from flask import request
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@babel.localeselector
+def get_locale():
+    # return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'es'

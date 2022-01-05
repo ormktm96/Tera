53ac247e311e1839769e05b19a19c4737a8fe1e8
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2022-01-05 12:25:25 -0500

    Update config.py
    Import a .env file with environment variables

--- config.py
+++ config.py
@@ -33,3 +33,12 @@ class Config(object):
 class Config(object):
     # ...
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    # ...

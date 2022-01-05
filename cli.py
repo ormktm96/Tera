b9658bff03ef5fbafaf11764167faac43f346b81
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2022-01-05 12:23:33 -0500

    Update cli.py
    Register custom application commands

--- cli.py
+++ cli.py
@@ -36,3 +36,28 @@ def init(lang):
             'pybabel init -i messages.pot -d app/translations -l ' + lang):
         raise RuntimeError('init command failed')
     os.remove('messages.pot')
+
+import os
+import click
+
+def register(app):
+    @app.cli.group()
+    def translate():
+        """Translation and localization commands."""
+        pass
+
+    @translate.command()
+    @click.argument('lang')
+    def init(lang):
+        """Initialize a new language."""
+        # ...
+
+    @translate.command()
+    def update():
+        """Update all languages."""
+        # ...
+
+    @translate.command()
+    def compile():
+        """Compile all languages."""
+        # ...

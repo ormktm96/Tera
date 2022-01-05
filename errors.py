074f5d4eb7c4f29b809f58134c8847bc1b437a1a
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2022-01-05 12:19:09 -0500

    Update errors.py
    Errors blueprint

--- errors.py
+++ errors.py
@@ -21,3 +21,9 @@ def not_found_error(error):
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
+
+from flask import Blueprint
+
+bp = Blueprint('errors', __name__)
+
+from app.errors import handlers

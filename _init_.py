46073d7afe28c551bfcd19d7bd81852495b794fe
ormktm96 <87331410+ormktm96@users.noreply.github.com>
2022-01-05 12:20:53 -0500

    Update _init_.py
    Application factory function

--- _init_.py
+++ _init_.py
@@ -161,3 +161,33 @@ from app import routes, models  # <-- remove errors from this import!
 from app.auth import bp as auth_bp
 app.register_blueprint(auth_bp, url_prefix='/auth')
 # ...
+
+# ...
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = _l('Please log in to access this page.')
+mail = Mail()
+bootstrap = Bootstrap()
+moment = Moment()
+babel = Babel()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    babel.init_app(app)
+
+    # ... no changes to blueprint registration
+
+    if not app.debug and not app.testing:
+        # ... no changes to logging setup
+
+    return app
